fc9c49e7a8c1e13b1778553efc8d99bb4609048f
Jordan Radushev <63496500+JordanRad@users.noreply.github.com>
2021-07-13 15:55:35 +0200

    init

---
+++ registry/src/test/java/limpo/registry/RegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package limpo.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
